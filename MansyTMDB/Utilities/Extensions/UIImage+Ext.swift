5658c822aa44eafffc114d720f0c6962e74c187a
Ahmed Elmansy <elmansy.me@icloud.com>
2022-12-01 12:40:20 +0200

    [Add] App Icon + Splashscreen + Placeholder

---
+++ MansyTMDB/Utilities/Extensions/UIImage+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIImage+Ext.swift
+//  MansyTMDB
+//
+//  Created by Ahmed Elmansy on 01/12/2022.
+//
+
+import UIKit
+
+extension UIImage{
+    
+    static var placeholder: UIImage?{
+        UIImage(named: "placeholder")
+    }
+    
+}
